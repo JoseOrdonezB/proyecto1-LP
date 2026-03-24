e89ca5da7dd8a37e7609560d6e562a43ff9ab80b
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-03-23 20:20:33 -0600

    manejo de strings y ejemplos de analizador y generador

--- src/codegen.py
+++ src/codegen.py
@@ -1,20 +1,16 @@
 def generate_lexer_code(dfa):
-
     transitions = {}
     accepting = {}
 
     for sid, state in dfa.states.items():
         transitions[sid] = state.transitions
-
         if state.accepting_token:
             accepting[sid] = {
                 "token": state.accepting_token,
                 "action": state.accepting_action
             }
 
-    code = f"""
-class Lexer:
-
+    code = f'''class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
@@ -25,9 +21,7 @@ class Lexer:
     START_STATE = {dfa.start_state}
 
     def next_token(self):
-
         while True:
-
             if self.pos >= self.length:
                 return None
 
@@ -37,35 +31,47 @@ class Lexer:
             last_pos = self.pos
 
             while self.pos < self.length:
-
                 char = self.text[self.pos]
 
-                if char not in self.TRANSITIONS.get(state, {{}}):
+                # --- Manejo de strings ---
+                if char == '"':
+                    start_str = self.pos
+                    self.pos += 1
+                    while self.pos < self.length:
+                        c = self.text[self.pos]
+                        if c == '"' and self.text[self.pos-1] != '\\\\':
+                            self.pos += 1
+                            lexeme = self.text[start_str:self.pos]
+                            return "STRING", lexeme
+                        self.pos += 1
+                    line = self.text[:start_str].count("\\n") + 1
+                    raise Exception(f"Error léxico: cadena no cerrada en línea {{line}}, posición {{start_str}}")
+
+                # --- DFA normal ---
+                state_trans = self.TRANSITIONS.get(state, {{}})
+                if char in state_trans:
+                    state = state_trans[char]
+                    self.pos += 1
+                    if state in self.ACCEPTING:
+                        last_accept = state
+                        last_pos = self.pos
+                else:
                     break
 
-                state = self.TRANSITIONS[state][char]
-                self.pos += 1
-
-                if state in self.ACCEPTING:
-                    last_accept = state
-                    last_pos = self.pos
-
             if last_accept is None:
+                line = self.text[:self.pos].count("\\n") + 1
                 error_char = self.text[self.pos] if self.pos < self.length else "EOF"
-                raise Exception(f"Error léxico en posición {{self.pos}}: '{{error_char}}'")
+                raise Exception(f"Error léxico en línea {{line}}, posición {{self.pos}}: '{{error_char}}'")
 
             lexeme = self.text[start:last_pos]
             self.pos = last_pos
 
             token_info = self.ACCEPTING[last_accept]
             token = token_info["token"]
-
-            # limpiar token solo si viene de acción tipo {{ TOKEN }}
             if token:
                 token = token.replace("return", "").replace(";", "").strip()
                 token = token.strip("{{}} ").strip()
 
-            # ignorar whitespace sin recursión
             if token in ["WHITESPACE", "EOL"]:
                 continue
 
@@ -79,6 +85,5 @@ class Lexer:
                 break
             tokens.append(tok)
         return tokens
-"""
-
+'''
     return code
